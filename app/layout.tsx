6cf80fdfe5b46c0ac4189ecec3eea050504cf544
Avery <avery@bluebirdback.com>
2024-05-25 15:46:04 +0800

    feat: Chapter 6 - Setting Up Your Database

---
+++ app/layout.tsx
@@ -0,0 +1,14 @@
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} antialiased bg-slate-100`}>{children}</body>
+    </html>
+  );
+}
